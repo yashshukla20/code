9800bc9b790a9963c0c7664375ad77c6aaed3d50
Yash Shukla <127749722+yashshukla20@users.noreply.github.com>
2023-09-26 12:00:30 +0000

    commit-message

---
+++ save.java
@@ -0,0 +1,20 @@
+@yashshukla20 ➜ /workspaces/code (main) $ git add .
+@yashshukla20 ➜ /workspaces/code (main) $ git commit -m "commit-message"
+[main 977d84b] commit-message
+ 1 file changed, 36 insertions(+)
+ create mode 100644 zerotolerence.java
+@yashshukla20 ➜ /workspaces/code (main) $ git commit --amend --date="2023-09-26 12:00:30"
+[main ab87e63] commit-message
+ Date: Tue Sep 26 12:00:30 2023 +0000
+ 1 file changed, 36 insertions(+)
+ create mode 100644 zerotolerence.java
+@yashshukla20 ➜ /workspaces/code (main) $ git push
+Enumerating objects: 3, done.
+Counting objects: 100% (3/3), done.
+Delta compression using up to 2 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (2/2), 278 bytes | 278.00 KiB/s, done.
+Total 2 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/yashshukla20/code
+   4661c8e..ab87e63  main -> main
